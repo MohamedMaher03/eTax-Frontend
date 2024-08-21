c2fc666386cf412eab7cb244aaebeb9db25149a8
Farah Khaled <farahkhaled03@gmail.com~>
2024-08-21 11:55:51 +0300

    Login Form completed

--- OneDrive/Desktop/large projects/etax-frontend/etax/src/App.js
+++ OneDrive/Desktop/large projects/etax-frontend/etax/src/App.js
@@ -3,17 +3,16 @@ import './App.css';
 import RegistrationForm from './Components/RegistrationForm/RegistrationForm';
 import { Route, Routes, Router } from 'react-router-dom';
 import CompleteRegistrationForm from './Components/RegistrationForm/CompleteRegistrationForm';
+import LoginForm from './Components/LoginForm/LoginForm';
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Routes>
+        <Route path='/login' element={<LoginForm/>}/>
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/complete-register" element={<CompleteRegistrationForm />} />
       </Routes>
-=======
->>>>>>> 581c167f8d591756c8dae54546a1bd7a2b03b6a4
     </>
   );
 }
